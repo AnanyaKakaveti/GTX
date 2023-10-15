4d3ac22766ec16a9c500ea2a59ca44d4d28765eb
amet407 <ametoje.virdi@gmail.com>
2023-10-14 23:21:25 -0400

    Improved home screen

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,22 +1,51 @@
 // import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
 
 const Home = () => { 
-
+    const [espanol, setEspanol] = useState(false)
+    const [espanolpop, setEspanolpop] = useState(false)
     return (
       <div className="App">
       <header>
           <div className='App-header'>
-            Welcome!
+            <div className='first-line'>Learning language through music</div>
+            Welcome to Cantar! <br></br>
+            <div className='selection'>Select your language:</div>
+            {/* Select your language: */}
+            {/* <Link to="/play"> */}
+            <div className = 'button-line'>
+              <button onClick={()=>setEspanol(true)} class="button-27" role="button">Spanish</button>
+              <span></span>
+              <button class="button-27" role="button">German</button>
+              <span></span>
+              <button class="button-27" role="button">French</button>
+            </div>
+          {/* </Link> */}
+          <div className='selection'>Select your genre:</div>
+          <div className = 'button-line'>
+              <button onClick={()=>setEspanolpop(true)} class="button-27" role="button">Pop</button>
+              <span></span>
+              <button class="button-27" role="button">Rap</button>
+              <span></span>
+              <button class="button-27" role="button">Hip Hop</button>
+            </div>
+            <div className='selection'>Select your song:</div>
             <Link to="/play">
-            <button>
-              Go to Play Page!
-            </button>
-          </Link>
+            <div className = 'button-line'>
+              
+              {espanol&&espanolpop?<button class="button-27" role="button">Suerte - Shakira</button>:null}
+              <span></span>
+              {espanol&&espanolpop?<button class="button-27" role="button">Un Preview - Bad Bunny</button>:null}
+              <span></span>
+              {espanol&&espanolpop?<button class="button-27" role="button">Holanda - Jhayco</button>:null}
+              
+            </div>
+            </Link>
           </div>
       </header>
     </div>
-    
+
     )
 }
 export default Home;
